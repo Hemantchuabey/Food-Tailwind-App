5bdf8bc6bf4a347bb8735ab251b39e8a5ec69e35
Hemant <hementchaubey@gmail.com>
2023-02-01 20:50:44 +0530

    applied styling to hero component.

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -2,18 +2,19 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <div>
+    <div className="max-w-[1640px] p-4 mx-auto">
+      <div className="max-h-[500px] relative ">
         {/* Overlay */}
-        <div>
-          <h1>
-            The <span>Best</span>
+        <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center">
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            The <span className="text-orange-500">Best</span>
           </h1>
-          <h1>
-            Foods <span>Delivered</span>
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            <span className="text-orange-500">Foods</span>Delivered
           </h1>
         </div>
         <img
+          className="w-full max-h-[500px] object-cover "
           src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt="/"
         />
