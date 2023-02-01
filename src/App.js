5d4a5fbea953e61e3d521db6b468b2661daddb77
Hemant <hementchaubey@gmail.com>
2023-02-01 21:27:02 +0530

    Created HeadlineCard component applied styling.created one card which show one of special product.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Headline from "./Component/HeadlineCards";
 import Hero from "./Component/Hero";
 import Navbar from "./Component/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <Headline />
     </div>
   );
 }
