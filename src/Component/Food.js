e36c1f8ec82ffb4d8f2ce050edbb032f574b8739
Hemant <hementchaubey@gmail.com>
2023-02-02 19:34:40 +0530

    Added Filter content to the Food component. Filter can be of two type Filter by Type and Filter by Price.

--- src/Component/Food.js
+++ src/Component/Food.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { data } from "../Data/Data";
 
 const Food = () => {
-  return <div>Food</div>;
+  console.log(data);
+  return (
+    <div>
+      <h1>Top Rated Menu Items</h1>
+      {/* filter row */}
+      <div>
+        {/* filter type */}
+        <div>
+          <p>Filter Type</p>
+          <button>All</button>
+          <button>Burgers</button>
+          <button>Pizza</button>
+          <button>Salads</button>
+          <button>Chicken</button>
+        </div>
+        {/* filter price */}
+        <div>
+          <p>Filter Type</p>
+          <div>
+            <button>$</button>
+            <button>$$</button>
+            <button>$$$</button>
+            <button>$$$$</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Food;
