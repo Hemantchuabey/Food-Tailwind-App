01b67ef1c940033455c683f58ad11f63fa0d34dc
Hemant <hementchaubey@gmail.com>
2023-02-03 20:45:22 +0530

    added filterType function to the button to filter and display the page base on data type.

--- src/Component/Food.js
+++ src/Component/Food.js
@@ -33,19 +33,34 @@ const Food = () => {
         <div>
           <p className="font-bold text-gray-700">Filter Type</p>
           <div className="flex justify-between flex-wrap">
-            <button className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1">
+            <button
+              onClick={() => setFoods(data)}
+              className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1"
+            >
               All
             </button>
-            <button className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1">
+            <button
+              onClick={() => filterType("burger")}
+              className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1"
+            >
               Burgers
             </button>
-            <button className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1">
+            <button
+              onClick={() => filterType("pizza")}
+              className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1"
+            >
               Pizza
             </button>
-            <button className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1">
+            <button
+              onClick={() => filterType("salad")}
+              className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1"
+            >
               Salads
             </button>
-            <button className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1">
+            <button
+              onClick={() => filterType("chicken")}
+              className="border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-300 m-1"
+            >
               Chicken
             </button>
           </div>
