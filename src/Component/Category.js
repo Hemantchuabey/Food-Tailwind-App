d0045ae13198c31054ffa3770925caf27098fce1
Hemant <hementchaubey@gmail.com>
2023-02-04 19:45:51 +0530

    Applied styling to category component.

--- src/Component/Category.js
+++ src/Component/Category.js
@@ -4,15 +4,24 @@ import { categories } from "../Data/Data";
 const Category = () => {
   //   console.log(data);
   return (
-    <div>
-      <h1>Top Rated Menu Items</h1>
+    <div className="max-w-[1640px] px-4 py-12 m-auto ">
+      <h1 className="text-orange-600 font-bold text-4xl text-center">
+        Top Rated Menu Items
+      </h1>
       {/* Category */}
-      <div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
         {categories.map((item, index) => {
           return (
-            <div key={index}>
-              <h2>{item.name}</h2>
-              <img src={item.image} alt={item.name} />
+            <div
+              key={index}
+              className="bg-gray-100 rounded-lg p-4 flex justify-between items-center cursor-pointer hover:scale-105 duration-300 ease-in-out"
+            >
+              <h2 className="font-bold sm:text-xl">{item.name}</h2>
+              <img
+                src={item.image}
+                alt={item.name}
+                className="w-20 object-cover    "
+              />
             </div>
           );
         })}
